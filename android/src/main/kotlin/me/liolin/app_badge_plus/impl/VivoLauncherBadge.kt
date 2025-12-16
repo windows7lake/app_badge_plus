81f9883b0b5ead0b9bd5d2b7222d976f4ac2da31
LioLin <windows7lake@gmail.com>
2025-12-16 15:01:29 +0800

    fix: vivo funtouch os no showing badge num

--- android/src/main/kotlin/me/liolin/app_badge_plus/impl/VivoLauncherBadge.kt
+++ android/src/main/kotlin/me/liolin/app_badge_plus/impl/VivoLauncherBadge.kt
@@ -22,10 +22,31 @@ class VivoLauncherBadge : IBadge {
         intent.putExtra(INTENT_EXTRA_PACKAGE_NAME, context.packageName)
         intent.putExtra(INTENT_EXTRA_CLASS_NAME, LauncherTool.getClassName(context))
         intent.putExtra(INTENT_EXTRA_BADGE_COUNT, count)
+        if (Intent::class.java.canonicalName != null) {
+            intent.addFlags(
+                invokeIntConstants(
+                    Intent::class.java.canonicalName!!,
+                    "FLAG_RECEIVER_INCLUDE_BACKGROUND",
+                    0
+                )
+            )
+        }
         context.sendBroadcast(intent)
     }
 
     override fun getSupportLaunchers(): List<String> {
-        return listOf("com.vivo.launcher")
+        return listOf("com.vivo.launcher", "com.vivo.simplelauncher")
+    }
+
+    fun invokeIntConstants(canonicalName: String, name: String, defaultValue: Int): Int {
+        var value = defaultValue
+        try {
+            val c = Class.forName(canonicalName)
+            val field = c.getField(name)
+            value = field.getInt(c)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return value
     }
 }
